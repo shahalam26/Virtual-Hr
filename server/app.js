bdcbaad59d1df08ca64417fea7ba9ac2797e8aad
shahji <mdshahalamsabri78601@gmail.com>
2026-05-09 09:13:13 +0530

    Fix CORS policy to allow Vercel deployment URL

--- server/app.js
+++ server/app.js
@@ -14,7 +14,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: [
+    process.env.CLIENT_URL, 
+    "http://localhost:5173", 
+    "https://virtualinterviewer-eta.vercel.app"
+  ].filter(Boolean),
   credentials: true
 }));
 
